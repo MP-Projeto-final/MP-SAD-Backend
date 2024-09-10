1ba0de3694699ca668116256a1b391dc03bfb191
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-10 19:30:21 -0300

    Update donations.router.js

--- src/routers/donations.router.js
+++ src/routers/donations.router.js
@@ -1,16 +1,19 @@
 import express from 'express';
-import { createDonation, getDonationsByUser, getDonationById, getPacoteQrCode } from '../controllers/donations.controller.js';
+import { createDonation, getDonationsByUser, getDonationById, getPacoteQrCode, uploadMedia, getMediaByPackageId } from '../controllers/donations.controller.js';
 import { schemaValidation } from '../middlewares/schema.validation.js';
 import { tokenValidation } from '../middlewares/token.validation.js';
 import { createDonationSchema } from '../schemas/donations.schema.js';
+import multer from "multer";
 
 const donationRouter = express.Router();
+const upload = multer();
 
 donationRouter.use(tokenValidation); 
-// donationRouter.post('/', schemaValidation(createDonationSchema), createDonation);
 donationRouter.get('/', getDonationsByUser);
 donationRouter.get('/:id', getDonationById);
 donationRouter.post('/donations', tokenValidation, createDonation);
 donationRouter.get('/pacotes/:id/qrcode', getPacoteQrCode);
+donationRouter.post('/media/upload', upload.single('imagem'), uploadMedia);
+donationRouter.get('/media/:pacoteId', getMediaByPackageId);
 
 export default donationRouter;
