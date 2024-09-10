97581d03a385463aef21da2da6c10dcfa1d4fce9
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-10 19:21:20 -0300

    Update donations.controller.js

--- src/controllers/donations.controller.js
+++ src/controllers/donations.controller.js
@@ -1,18 +1,5 @@
 import * as donationService from "../services/donations.service.js";
 
-// export async function createDonation(req, res) {
-//     const { descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado } = req.body;
-//     const userId = res.locals.user.id; 
-//     console.log("ID do usuário autenticado para criar a doação:", userId);
-
-//     try {
-//         const donation = await donationService.createDonation(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado);
-//         res.status(201).send(donation);
-//     } catch (error) {
-//         res.status(error.status || 500).send(error.message);
-//     }
-// }
-
 export async function getPacoteQrCode(req, res) {
     const { id } = req.params;
 
@@ -31,16 +18,13 @@ export async function createDonation(req, res) {
     const userId = res.locals.user.id;
 
     try {
-        // Chama o serviço para criar a doação e o pacote
         const { donation, pacote } = await donationService.createDonationWithPackage(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado);
         
-        // Envia a resposta com a doação, pacote e QR code já em base64
         res.status(201).json({ donation, pacote, qrCode: `data:image/png;base64,${pacote.qrcode}` });
     } catch (error) {
         res.status(error.status || 500).send(error.message);
     }
 }
-
 
 export async function getDonationsByUser(req, res) {
     const userId = res.locals.user.id; 
@@ -66,5 +50,30 @@ export async function getDonationById(req, res) {
         res.json(donationData);
     } catch (error) {
         res.status(500).json({ message: error.message });
+    }
+}
+
+export async function uploadMedia(req, res) {
+    const { pacoteId } = req.body;
+    const tipo = req.file.mimetype;  
+    const imagemBuffer = req.file.buffer; 
+  
+    try {
+      const midia = await donationService.createMedia(pacoteId, tipo, imagemBuffer);
+      res.status(201).json(midia);  
+    } catch (error) {
+      res.status(error.status || 500).json({ message: error.message });
+    }
+  }
+  
+
+export async function getMediaByPackageId(req, res) {
+    const { pacoteId } = req.params;
+  
+    try {
+      const midias = await donationService.getMediaByPackageId(pacoteId);
+      res.status(200).json(midias);
+    } catch (error) {
+      res.status(error.status || 500).json({ message: error.message });
     }
 }
