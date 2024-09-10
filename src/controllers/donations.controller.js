84525c0feb3c1f8f3cc2be433dffc40ecc03c726
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-10 15:46:25 -0300

    Update donations.controller

--- src/controllers/donations.controller.js
+++ src/controllers/donations.controller.js
@@ -1,13 +1,25 @@
 import * as donationService from "../services/donations.service.js";
 
+// export async function createDonation(req, res) {
+//     const { descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado } = req.body;
+//     const userId = res.locals.user.id; 
+//     console.log("ID do usuário autenticado para criar a doação:", userId);
+
+//     try {
+//         const donation = await donationService.createDonation(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado);
+//         res.status(201).send(donation);
+//     } catch (error) {
+//         res.status(error.status || 500).send(error.message);
+//     }
+// }
+
 export async function createDonation(req, res) {
     const { descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado } = req.body;
     const userId = res.locals.user.id; 
-    console.log("ID do usuário autenticado para criar a doação:", userId);
 
     try {
-        const donation = await donationService.createDonation(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado);
-        res.status(201).send(donation);
+        const { donation, pacote } = await donationService.createDonationWithPackage(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado);
+        res.status(201).json({ donation, pacote, qrCode: pacote.qrcode });
     } catch (error) {
         res.status(error.status || 500).send(error.message);
     }
--- src/services/donations.service.js
+++ src/services/donations.service.js
@@ -1,4 +1,5 @@
 import * as donationRepository from "../repository/donations.repository.js";
+import QRCode from 'qrcode';
 
 export async function createDonation(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado) {
     return await donationRepository.createDonation(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado);
@@ -16,4 +17,15 @@ export async function getDonationById(donationId) {
         throw error;
     }
     return donation;
+}
+
+export async function createDonationWithPackage(userId,descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado) {
+    const donation = await donationRepository.createDonation(userId, descricao, destino_cep, destino_rua, destino_numero, destino_complemento, destino_bairro, destino_cidade, destino_estado);
+    
+    const trackingLink = `http://localhost:3000/tracking/${donation.id}`;  
+    const qrCode = await QRCode.toDataURL(trackingLink);  
+   
+    const pacote = await donationRepository.createPacote(donation.id, qrCode, 'Criado');
+
+    return { donation, pacote };
 }
