c94ee8588a50646e8ab3c1dbad236657833e4a10
Nathália Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-09-10 21:05:56 -0300

    Update donations.router.js

--- src/routers/donations.router.js
+++ src/routers/donations.router.js
@@ -8,13 +8,13 @@ import multer from "multer";
 const donationRouter = express.Router();
 const upload = multer();
 
-donationRouter.get('/my', tokenValidation, getDonationsByUser);
-donationRouter.get('/:id', tokenValidation, getDonationById);
-donationRouter.post('/donations', tokenValidation, createDonation);
 donationRouter.get('/pacotes/:id/qrcode', getPacoteQrCode);
 donationRouter.post('/media/upload', upload.single('imagem'), uploadMedia);
 donationRouter.get('/media/:pacoteId', getMediaByPackageId);
 donationRouter.put('/pacotes/:id/status', upload.single('imagem'), updatePacoteStatusAndUploadMedia);
-donationRouter.get('/statistics', getStatistics);
+donationRouter.get('/stats', getStatistics);
+donationRouter.get('/my', tokenValidation, getDonationsByUser);
+donationRouter.get('/:id', tokenValidation, getDonationById);
+donationRouter.post('/donations', tokenValidation, createDonation);
 
 export default donationRouter;
